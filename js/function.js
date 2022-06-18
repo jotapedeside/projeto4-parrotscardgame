e10855aa709443507c107005b2d4f60a5ec22af6
jotapedeside <jotapedeside@gmail.com>
2022-06-18 17:31:31 -0300

    feature(function.js): flip card implemented

--- js/function.js
+++ js/function.js
@@ -1,2 +1,23 @@
-const cartas = prompt("Escolha um número de cartas entre 4 e 14");
-console.log(cartas);
+const cards = document.querySelectorAll(".card");
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  secondCard = this;
+}
+cards.forEach((card) => card.addEventListener("click", flipCard));
